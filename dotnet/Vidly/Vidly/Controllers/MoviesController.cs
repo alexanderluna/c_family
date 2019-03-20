dde8a2305ba17f4c8f92ec95c442f312cab3235d
Mycroft1891 <mycroft1891@gmail.com>
2019-03-20 10:03:34 +0100

    Update: random action inside movie controller

--- dotnet/Vidly/Vidly/Controllers/MoviesController.cs
+++ dotnet/Vidly/Vidly/Controllers/MoviesController.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: /<controller>/
-        public IActionResult Index()
+        public IActionResult Random()
         {
-            return View();
+            var movie = new Movie() { Name = "Conan" };
+            return View(movie);
         }
     }
 }
