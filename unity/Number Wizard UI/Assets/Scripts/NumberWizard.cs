12398c21df229b2fa34ab879b837ad2320a0845d
Mycroft1891 <mycroft1891@gmail.com>
2019-12-30 03:14:49 +0100

    Add: refactored NumberWizard logic and hook up with UI elements

---
+++ unity/Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int min = 1;
+    [SerializeField] int max = 1000;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess = 500;
+
+
+    void Start()
+    {
+        StartGame();
+    }
+
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max);
+        guessText.text = guess.ToString();
+    }
+
+
+    public void OnPressLower()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+
+
+    public void OnPressHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+}
