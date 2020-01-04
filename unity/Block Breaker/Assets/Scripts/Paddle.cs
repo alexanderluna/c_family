c6d8c9f23a102c6fb04d7e8700cba30a68e205a1
Mycroft1891 <mycroft1891@gmail.com>
2020-01-04 01:47:56 +0100

    Add: script to update the paddle's position based on the mouse's position without going out of bound

---
+++ unity/Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float screenWidthInUnits = 16f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float mousePosition = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+        paddlePosition.x = Mathf.Clamp(mousePosition, minX, maxX);
+        transform.position = paddlePosition;
+    }
+}
