2d026d10fb2081746fcae86af02798312647851b
Mycroft1891 <mycroft1891@gmail.com>
2020-01-12 13:12:13 +0100

    Add: Ball script to attach the ball's and paddle's position at the beginning of the game

---
+++ unity/Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle;
+
+    Vector2 paddleToBallVector;
+
+
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle.transform.position;
+    }
+
+
+    void Update()
+    {
+        Vector2 paddlePosition = new Vector2(
+            paddle.transform.position.x,
+            paddle.transform.position.y
+        );
+
+        transform.position = paddlePosition + paddleToBallVector;
+    }
+}
