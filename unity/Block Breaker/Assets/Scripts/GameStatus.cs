671e8562bfdadb1864f11c3792223f7768015833
Mycroft1891 <mycroft1891@gmail.com>
2020-01-31 18:29:43 +0100

    Update: display game score in the UI

--- unity/Block Breaker/Assets/Scripts/GameStatus.cs
+++ unity/Block Breaker/Assets/Scripts/GameStatus.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1f;
+    [SerializeField] TextMeshProUGUI scoreText;
     [SerializeField] int pointsPerBlockDestroyed = 64;
     [SerializeField] int currentScore = 0;
+
+
+    private void Start()
+    {
+        SetScoreText();   
+    }
 
 
     private void Update()
@@ -15,8 +23,15 @@ public class GameStatus : MonoBehaviour
     }
 
 
+    private void SetScoreText()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
+
     public void AddScore()
     {
         currentScore += pointsPerBlockDestroyed;
+        SetScoreText();
     }
 }
