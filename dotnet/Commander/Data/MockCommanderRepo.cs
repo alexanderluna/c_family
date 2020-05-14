8579044de88f30c4cafa06c56c6023870c521b6d
Mycroft1891 <mycroft1891@gmail.com>
2020-05-14 15:31:45 +0200

    Add command model and repo to mock get requests with dummy data

---
+++ dotnet/Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { Id = 0, HowTo = "Do an echo", Line = "Write echo", Platform = "unix" },
+                new Command { Id = 1, HowTo = "Do an echo", Line = "Write echo", Platform = "unix" },
+                new Command { Id = 2, HowTo = "Do an echo", Line = "Write echo", Platform = "unix" },
+                new Command { Id = 3, HowTo = "Do an echo", Line = "Write echo", Platform = "unix" }
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Do an echo", Line = "Write echo", Platform = "unix" };
+        }
+    }
+}
